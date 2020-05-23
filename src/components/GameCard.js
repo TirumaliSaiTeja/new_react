5a8583446ebd465c48d2d839ece19ed1c5bbe1ff
saiteja <tsaiteja7@gmail.com>
2020-05-23 23:30:59 +0530

    added game card

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,8 +1,25 @@
 import React from "react";
 
 const GameCard = () => (
-  <div>
-    <h1>Game Card</h1>
+  <div className="ui card">
+    <div className="image">
+      <span className="ui green ribbon label">$43.43</span>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSW68T5TAd6U9ZgreKMKSDG5iwfuSPJswtwfaR2Om6_w0XNYQEK&usqp=CAU"
+        alt="Game cover"
+      />
+    </div>
+    <div className="content">
+      <a href="#" className="header">
+        "Battle field"
+      </a>
+      <div className="meta">
+        <i className="icon-users" />
+        2-4&nbsp;
+        <i className="icon-wait" />
+        60mins;
+      </div>
+    </div>
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
 import "semantic-ui-css/semantic.min.css";
-//import GameCard from "./components/GameCard.js";
-
-const GameCard = function() {
-  return (
-    <div>
-      <h1>Game Card</h1>
-    </div>
-  );
-};
+import GameCard from "./components/GameCard.js";
 
 render(<GameCard />, document.getElementById("root"));
