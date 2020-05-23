afb0c33ec10abc26c83c1cd18d1c686494a88af9
saiteja <tsaiteja7@gmail.com>
2020-05-23 22:10:41 +0530

    changes in component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
 import "semantic-ui-css/semantic.min.css";
-import GameCard from "./components/GameCard.js";
+//import GameCard from "./components/GameCard.js";
+
+const GameCard = function() {
+  return (
+    <div>
+      <h1>Game Card</h1>
+    </div>
+  );
+};
 
 render(<GameCard />, document.getElementById("root"));
